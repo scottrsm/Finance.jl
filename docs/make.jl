944628d1240aa94ff0fa512d14337324237104b8
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:39:07 -0400

    Added github doc and test actions.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Finance
+using Finance
+import Pkg
 
-makedocs(sitename="Finance Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Finance",
+	format = Documenter.HTML(),
+	modules = [Finance]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Finance.jl.git"
+	)
